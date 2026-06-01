12fb0a6bff25e436ee28d5605eeaf508f96e57d8
Aprillia <makoaprillia@gmail.com>
2026-06-02 06:20:34 +0800

    fog: kernel make while build true
    Signed-off-by: Aprillia <makoaprillia@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 #
 
 DEVICE_PATH := device/xiaomi/fog
-KERNEL_PATH := device/xiaomi/fog-kernel
+KERNEL_PATH := device/xiaomi/kernel/xiaomi/sm6225
 
 # A/B
 BOARD_USES_RECOVERY_AS_BOOT := true
@@ -86,7 +86,7 @@ HWUI_COMPILE_FOR_PERF := true
 
 # Kernel
 BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_IMAGE_NAME  := Image
+BOARD_KERNEL_IMAGE_NAME  := Image.gz
 BOARD_KERNEL_OFFSET      := 0x00008000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_RAMDISK_OFFSET     := 0x01000000
@@ -111,11 +111,10 @@ BOARD_KERNEL_CMDLINE += \
     kpti=off \
     kasan=off
 
-TARGET_NO_KERNEL_OVERRIDE := true
-TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
-BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
-BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
-BOARD_MKBOOTIMG_ARGS += --dtb $(BOARD_PREBUILT_DTBIMAGE_DIR)/khaje.dtb
+TARGET_NO_KERNEL_OVERRIDE := false
+TARGET_KERNEL_SOURCE := $(KERNEL_PATH)
+TARGET_KERNEL_CONFIG := vendor/bengal-perf.defconfig vendor/xiaomi/fog.config
+BOARD_MKBOOTIMG_ARGS += --dtb $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm64/boot/dts/vendor/qcom/khaje.dtb 
 
 # Media
 TARGET_USES_ION := true
